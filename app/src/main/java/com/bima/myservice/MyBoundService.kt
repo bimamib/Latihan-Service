d5ef38ba8eeefc1133a7886146d374e5ff013d9f
bimamib <bimaprast1601@gmail.com>
2023-10-09 23:24:57 +0700

    menerapkan metode onBinding, karena bound service mengikat sebuah object

--- app/src/main/java/com/bima/myservice/MyBoundService.kt
+++ app/src/main/java/com/bima/myservice/MyBoundService.kt
@@ -4,17 +4,36 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MyBoundService : Service() {
 
     companion object {
+        private val serviceJob = Job()
+        private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+        val numberLiveData: MutableLiveData<Int> = MutableLiveData()
         private val TAG = MyBoundService::class.java.simpleName
     }
 
     private var binder = MyBinder()
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Log.d(TAG, "onBind: ")
+        serviceScope.launch {
+            for (i in 1..50) {
+                delay(1000)
+                Log.d(TAG, "Do Something $i")
+                numberLiveData.postValue(i)
+            }
+            Log.d(TAG, "Service dihentikan")
+        }
+        return binder
     }
 
     internal inner class MyBinder : Binder() {
