46dbd9a0091b193897fb9b3d570775d076452746
bimamib <bimaprast1601@gmail.com>
2023-10-09 23:19:59 +0700

    membuat kelas MyBinder yang mewarisi kelas Binder dalam kelas MyBoundService.kt

--- app/src/main/java/com/bima/myservice/MyBoundService.kt
+++ app/src/main/java/com/bima/myservice/MyBoundService.kt
@@ -2,11 +2,22 @@ package com.bima.myservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 
 class MyBoundService : Service() {
 
+    companion object {
+        private val TAG = MyBoundService::class.java.simpleName
+    }
+
+    private var binder = MyBinder()
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    internal inner class MyBinder : Binder() {
+        val getService: MyBoundService = this@MyBoundService
     }
 }
