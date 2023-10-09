5fa6b20388a4704b99b491b7448bc195d5d4baa0
bimamib <bimaprast1601@gmail.com>
2023-10-10 00:29:16 +0700

    menerapkan fungsionalitas untuk melepas ikatan

--- app/src/main/java/com/bima/myservice/MyBoundService.kt
+++ app/src/main/java/com/bima/myservice/MyBoundService.kt
@@ -15,13 +15,14 @@ import kotlinx.coroutines.launch
 class MyBoundService : Service() {
 
     companion object {
-        private val serviceJob = Job()
-        private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
-        val numberLiveData: MutableLiveData<Int> = MutableLiveData()
         private val TAG = MyBoundService::class.java.simpleName
     }
 
     private var binder = MyBinder()
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    val numberLiveData: MutableLiveData<Int> = MutableLiveData()
 
     override fun onBind(intent: Intent): IBinder {
         Log.d(TAG, "onBind: ")
@@ -34,6 +35,22 @@ class MyBoundService : Service() {
             Log.d(TAG, "Service dihentikan")
         }
         return binder
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        Log.d(TAG, "onUnbind: ")
+        serviceJob.cancel()
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent) {
+        super.onRebind(intent)
+        Log.d(TAG, "onRebind: ")
     }
 
     internal inner class MyBinder : Binder() {
