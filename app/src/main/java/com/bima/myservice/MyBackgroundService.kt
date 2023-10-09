f3f5bef4e3fecf60ae0e8888c03986062e5d5340
bimamib <bimaprast1601@gmail.com>
2023-10-09 12:21:26 +0700

    melengkapi kode pada MyBackgroundService.kt

--- app/src/main/java/com/bima/myservice/MyBackgroundService.kt
+++ app/src/main/java/com/bima/myservice/MyBackgroundService.kt
@@ -3,10 +3,21 @@ package com.bima.myservice
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import java.lang.UnsupportedOperationException
 
 class MyBackgroundService : Service() {
 
+    companion object {
+        internal val TAG = MyBackgroundService::class.java.simpleName
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d(TAG, "Service dijalankan...");
+        return START_STICKY
     }
 }
