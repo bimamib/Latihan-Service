39b9ecf1a7b77b31a757a3849f2f6982ea513874
bimamib <bimaprast1601@gmail.com>
2023-10-09 12:14:48 +0700

    menghubungkan view ke dalam MainActivity.kt

--- app/src/main/java/com/bima/myservice/MainActivity.kt
+++ app/src/main/java/com/bima/myservice/MainActivity.kt
@@ -2,10 +2,15 @@ package com.bima.myservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bima.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
